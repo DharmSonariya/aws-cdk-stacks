9ee572b1f5d77450ef0134e5ffb4c6d208570a1a
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-02 07:41:24 +0000

    add functionality to create environments with the same stack

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ from aws_cdk_stacks.stacks.private import *
 from aws_cdk_stacks.aws_cdk_stacks_stack import AwsCdkStacksStack
 
 app = core.App()
-AwsCdkStacksStack(app, "aws-cdk-stacks")
+org = 'DevOps'
 
+environments = {
+    'pro': {
+        'env': core.Environment(region=REGION, account=ACCOUNT),
+        'ips': '10.1.0.0/16',
+        'mx_azs': 2,
+    },
+    'dev': {
+        'env': core.Environment(region=REGION, account=ACCOUNT),
+        'ips': '10.1.0.0/16',
+        'mx_azs': 2,
+    },
+}
+
+for key, value in environments.items():
+
+    awsCdkStacksStack = AwsCdkStacksStack(
+        app, f'{org}-{key}-AwsCdkStacksStack',
+        env=value.get('env'),
+        org=f'{org}',
+        environment=f'{key}',
+    )
 
 app.synth()
