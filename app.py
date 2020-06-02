7c99a739c8684cf928428806538b4fb2bfa07165
Dharm Sonariya <dharmsonariya@outlook.com>
2020-06-02 07:37:24 +0000

    Create and add private.py to .gitignore

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
-
+from aws_cdk_stacks.stacks.private import *
 from aws_cdk_stacks.aws_cdk_stacks_stack import AwsCdkStacksStack
-
 
 app = core.App()
 AwsCdkStacksStack(app, "aws-cdk-stacks")
+
 
 app.synth()
